f524c31267490ae3643814c416d73b5c2e455b4b
Lars Inge Reinsnos <lars.reinsnos@spv.no>
2019-01-27 15:36:43 +0100

    bugfix

--- GGJ2019_Project/Assets/Scripts/MainMenu.cs
+++ GGJ2019_Project/Assets/Scripts/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour {
     [SerializeField] string gameStartScene;
 
     void Start () {
+        Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
         GameState.Reset();
     }
